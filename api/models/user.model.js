bb210436c53704e4ed144759e00b4bb5daab8d5e
sidram403 <siddharthpatil9108@gmail.com>
2023-11-27 23:48:44 +0530

    updated npm version

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -12,6 +12,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    phone: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
     password: {
       type: String,
       required: true,
